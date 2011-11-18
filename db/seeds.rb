1e3cc53cc09f0295a210b16ba0c7b1aaa84efedf
asmega <asmega@ph-lee.com>
2011-11-18 13:21:48 +0000

    updated seed data for categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 ["Delivered", "Current Sprint", "Next Sprint", "Backlog"].each do |cat|
-  Category.find_or_create_by_name(cat)
+  Category.find_or_create_by_name_and_technical_name(cat, cat.underscore.downcase.gsub(" ", "_"))
 end
