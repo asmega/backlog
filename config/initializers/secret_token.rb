2c559ec5deee04e5a24823256b29fd52036499b0
asmega <asmega@ph-lee.com>
2011-11-18 10:53:52 +0000

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backlog::Application.config.secret_token = '040f314759ddbc53e8a92b51ea43f7828919ac146000c0b0aa6a32f635323dd14ad9cf8235350f2c5ca7946be79fb1a876c0257c4f79e9c2ae5047451bb77bea'
