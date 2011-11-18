e6b694082b6aff686402924300beccb097414e29
asmega <asmega@ph-lee.com>
2011-11-18 12:06:41 +0000

    migration to add cat to inventory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111118114607) do
+ActiveRecord::Schema.define(:version => 20111118115936) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20111118114607) do
     t.text     "data"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id", :default => 1, :null => false
   end
 
 end
