b9279e6161c73094e0f447c55230d20eceee4a36
asmega <asmega@ph-lee.com>
2011-11-18 16:07:35 +0000

    added relative dates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def date_in_words(date)
+    return "#{time_ago_in_words(date)} from now" if in_future?(date)
+    return "#{time_ago_in_words(date)} ago" if in_past?(date)
+    "Today"
+  end
+
+  private
+
+  def in_past?(date)
+    date < Date.today
+  end
+
+  def in_future?(date)
+    date > Date.today
+  end
 end
