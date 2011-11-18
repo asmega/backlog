2eca17107e883e972d00af862b7964dcc5f7be68
asmega <asmega@ph-lee.com>
2011-11-18 14:02:08 +0000

    root points to inventories. inventory index updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Backlog::Application.routes.draw do
-  resources :categories
-
+  root :to => "inventories#index"
   resources :inventories
+  resources :categories
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +52,6 @@ Backlog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
