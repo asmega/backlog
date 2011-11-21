e62f516befc7ebcd629136fb416a56e2288185cb
asmega <asmega@ph-lee.com>
2011-11-21 16:49:56 +0000

    Changed the order of categories in the seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-["Current Sprint", "Next Sprint", "Backlog", "Delivered"].each do |cat|
+["Backlog", "Current Sprint", "Next Sprint", "Complete"].each do |cat|
   Category.find_or_create_by_name_and_technical_name(cat, cat.underscore.downcase.gsub(" ", "_"))
 end
