b71891c05ac5b2f7bc534ec6958f1415d5e15cbd
asmega <asmega@ph-lee.com>
2011-11-21 17:11:31 +0000

    after ajax update category counts

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,12 +3,14 @@ $(function() {
   $(".tasks").sortable({
     connectWith: ".tasks",
     update: function(event, ui){
+      var category = $(this).closest("ol").attr("id");
       var order = $(this).closest(".tasks").sortable('serialize');
       $.ajax({
         type: "PUT",
         url: "/tasks/order",
-        data: order + "&category=" + $(this).closest("ol").attr("id")
+        data: order + "&category=" + category
       });
+      $("#" + category + "_count").html($(this).closest("ol").find("li").length);
     }
   });
   $(".tasks").disableSelection();
